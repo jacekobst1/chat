944ddb5ed5963de5d99a7253f54489bfc90488c2
Jacek Obst <j.obst@webwizards.pl>
2020-02-24 12:00:20 +0000

    Modify User model, users table migration, and LoginController (login only with email)

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -37,4 +40,25 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+        $user = $this->getOrCreateUser($request->get('email'));
+        Auth::loginUsingId($user->id);
+        return $this->sendLoginResponse($request);
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            $this->username() => 'required|string',
+        ]);
+    }
+
+    public function getOrCreateUser($email): User
+    {
+        return User::firstOrCreate(['email' => $email]);
+    }
+
 }
--- app/User.php
+++ app/User.php
@@ -15,18 +15,14 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $fillable = ['email'];
 
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $hidden = ['remember_token'];
 
     /**
      * The attributes that should be cast to native types.
